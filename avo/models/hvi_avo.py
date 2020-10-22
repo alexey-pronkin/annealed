67782e06bd25e307a772495cbf55284b360865fe
gipast <timur7002007@gmail.com>
2020-10-22 21:34:09 +0300

    hvi avo experiments

--- avo/models/hvi_avo.py
+++ avo/models/hvi_avo.py
@@ -8,21 +8,19 @@ import torch.nn as nn
 
 
 class HVIAVO(HVI):
-    def __init__(self, input_dimension, depth, target, lr=1e-3, batch_size=64, hidden_dimension=2, beta1=0.999,
+    def __init__(self, input_dimension, depth, targets, lr=1e-3, batch_size=64, hidden_dimension=2, beta1=0.999,
                  beta2=0.999, optimizer="adam", k=100):
-        super().__init__(input_dimension, target, lr, batch_size, beta1, beta2, optimizer, k)
+        super().__init__(input_dimension, targets[-1], lr, batch_size, beta1, beta2, optimizer, k)
         """
         in_dim - input dimension
         h_dim - hidden dimension of transition
         depth - number of transitions
         """
         self._depth = depth
-        self._alphas = np.linspace(1. / depth, 1., depth)
-        self._initial_target = SimpleNormal(target.device)
+        self._alphas = range(depth)
+        self._initial_target = SimpleNormal(targets[-1].device)
         self._transitions = nn.ModuleList([HVITransition(input_dimension, hidden_dimension) for _ in range(depth)])
-
-    def annealed_loss(self, alpha, z):
-        return (1 - alpha) * self._initial_target.E(z) + alpha * self._target.E(z)
+        self._transitions_targets = targets
 
     def forward(self, x):
         """
@@ -30,11 +28,11 @@ class HVIAVO(HVI):
         """
         previous_log_probability = torch.sum(torch.distributions.normal.Normal(0, 1).log_prob(x), dim=1)
         loss = torch.sum(torch.distributions.normal.Normal(0, 1).log_prob(x), dim=1)
-        for alpha, transition in zip(self._alphas, self._transitions):
+        for i in range(self._depth):
             previous_log_probability = previous_log_probability.detach()
             x = x.detach()
-            x, log_probability = transition(x)
-            annealed_loss = self.annealed_loss(alpha, x)
+            x, log_probability = self._transitions[i](x)
+            annealed_loss = self._transitions_targets[i].E(x)
             loss += annealed_loss + log_probability + previous_log_probability
             previous_log_probability += log_probability
         return x, loss / self._depth
