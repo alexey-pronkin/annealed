2edf785ba8a70dba63f68ed30404ff58d9a495a7
Mikhail Kurenkov <kurenkov1995@mail.ru>
2020-10-22 21:15:43 +0300

    Add VAE HVI AVO

--- avo/models/__init__.py
+++ avo/models/__init__.py
@@ -3,3 +3,4 @@ from .hvi_elbo import HVIELBO
 from .vae_gaussian import VaeGaussian
 from .nflows import HouseholderELBO, HouseholderAVO, IAFELBO, IAFAVO
 from .vae_hvi import VAEHVI
+from .vae_hvi_avo import VAEHVIAVO
